79c8f8aa555b3470622643fc14c4adf8b6a68c9e
susana.serban <susana.serban@casomes.ro>
2019-06-24 15:50:30 +0300

    continuare

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,5 +1,7 @@
 package org.fasttrackit;
 
+import java.time.LocalDate;
+
 /**
  * Hello world!
  *
@@ -23,6 +25,21 @@ public class App
 
         adopter.name="Popescu";
         adopter.money=300.50;
+
+         AnimalFood animalFood = new AnimalFood();
+
+         animalFood.name ="Royal Canin";
+         animalFood.price = 10.5;
+         animalFood.quantity = 10;
+         animalFood.expirationDate= LocalDate.of(2019 ,10,25);
+
+
+         animalFood.stock = 15;
+
+
+
+
+
 
     }
 }
